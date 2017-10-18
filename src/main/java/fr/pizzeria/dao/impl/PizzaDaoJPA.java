340e1b4e6d8e2ab9488afe0b55e83bd87a6822bb
Pauline JACOB <pauline.mille@gmail.com>
2017-10-18 22:53:22 +0200

    svg JPA

--- src/main/java/fr/pizzeria/dao/impl/PizzaDaoJPA.java
+++ src/main/java/fr/pizzeria/dao/impl/PizzaDaoJPA.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import fr.pizzeria.console.PizzeriaAdminConsoleApp;
 import fr.pizzeria.dao.IPizzaDao;
@@ -45,19 +47,45 @@ public class PizzaDaoJPA implements IPizzaDao {
 //	@Override
 	public boolean saveNewPizza(Pizza pizza) throws SavePizzaException {
 		EntityManager em = emf.createEntityManager();
-		return false;
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		em.persist(pizza);
+		et.commit();
+		
+		em.close();
+		return true;
 	}
 
 //	@Override
 	public boolean updatePizza(String codePizza, Pizza pizza) throws UpdatePizzaException {
-		return false;		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		TypedQuery<Pizza> query = em.createQuery("from pizza p where p.code = :code", Pizza.class);
+		em.setProperty("code", codePizza);
+		em.remove(query.getResultList().get(0)); //Recup et delete pizza
+		em.persist(pizza);
+		et.commit();
+		
+		em.close();
+		return true;		
 	}
 
 //	@Override
 	public boolean deletePizza(String codePizza) throws DeletePizzaException {
-		return false;		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		TypedQuery<Pizza> query = em.createQuery("from pizza p where p.code = :code", Pizza.class);
+		em.setProperty("code", codePizza);
+		em.remove(query.getResultList().get(0));
+		et.commit();
+		
+		em.close();
+		return true;		
 	}
 	
+	//Pour récupérer la catégorie des pizzas
 	public Categorie findCategorie(int id){
 		
 		EntityManager em = emf.createEntityManager();
